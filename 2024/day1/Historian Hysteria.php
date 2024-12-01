a3c5f9858a49791af12839c25ff96065acca9b69
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-12-01 23:20:52 +0100

    Add comments

--- 2024/day1/Historian Hysteria.php
+++ 2024/day1/Historian Hysteria.php
@@ -5,23 +5,34 @@ require_once './../../Misc.php';
 runOnInputFile(function ($file) {
     $numbersLeft = [];
     $numbersRight = [];
+
+    // parse the file line by line
     while ($line = fgets($file)) {
         [$numberLeft, $numberRight] = explode('   ', $line);
         $numbersLeft[] = (int)$numberLeft;
         $numbersRight[] = (int)$numberRight;
     }
 
+    // sort both arrays
     sort($numbersLeft);
     sort($numbersRight);
 
+    // calculate the distance of each pair
     $distance = 0;
     foreach ($numbersLeft as $key => $number) {
         $distance += abs($number - $numbersRight[$key]);
     }
 
+    // result
     echo $distance;
 });
 
+/**
+ * @param array $numbers
+ * @return array
+ *
+ * Maybe I could implement this function to sort the array in a different way than using the built-in sort function.
+ */
 function customSort(array $numbers): array
 {
     // TODO
