80305da955b5508c495b37a05a38b3de8eaf38f1
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-12-02 20:38:57 +0100

    Solve part 1

---
+++ 2024/day2/Red-Nosed Reports.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once './../../Misc.php';
+
+runOnInputFile(static function ($file) {
+    $saveReports = 0;
+
+    while ($line = fgets($file)) {
+        $report = explode(' ', $line);
+
+        // determine if the report needs to be ascended or descended
+        $orderFactor = $report[0] < $report[1] ? 1 : -1;
+
+        for ($i = 1, $iMax = count($report); $i < $iMax; $i++) {
+            $distance = $orderFactor * ($report[$i] - $report[$i - 1]);
+            if ($distance < 1 || $distance > 3) {
+                continue 2;
+            }
+        }
+
+        $saveReports++;
+    }
+
+    echo $saveReports;
+});
