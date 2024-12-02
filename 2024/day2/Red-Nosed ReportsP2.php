0306f875c751c708d6cafaaa29b85c94fcdcdf0c
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-12-02 23:34:11 +0100

    Remove echoes

--- 2024/day2/Red-Nosed ReportsP2.php
+++ 2024/day2/Red-Nosed ReportsP2.php
@@ -8,68 +8,41 @@ runOnInputFile(static function ($file): void
 
     while ($line = fgets($file)) {
         $report = explode(' ', trim($line));
-        $line = trim($line);
-
-        echo "-----------------\n";
-        echo "Checking line [$line]\n";
-        for ($i = 1, $iMax = count($report); $i < $iMax; $i++) {
-            echo $report[$i] - $report[$i-1] . " ";
-        }
-        echo "\n";
 
         $result = checkReport($report);
 
-        if (is_int($result)) {
-            $val = $report[$result+1];
-            echo "Problem found: $report[$result] (#$result) and $val (#" . ($result + 1) . ")\n";
-        }
-
         if ($result !== true) {
             $reportA = $report;
-            echo "Trying to remove $report[$result] (#$result)\n";
             unset($reportA[$result]);
             $newResult = checkReport(array_values($reportA));
-            if ($newResult === true) {
-                $val = $report[$result];
-                echo "Removing $val (#$result)\n";
-            }
 
             if ($newResult !== true) {
                 $reportB = $report;
-                $val = $report[$result + 1];
-                echo "Trying to remove $val (#" . ($result + 1) . ")\n";
                 unset($reportB[$result + 1]);
                 $newResult = checkReport(array_values($reportB));
-                if ($newResult === true) {
-                    $val = $report[$result + 1];
-                    echo "Removing $val (#$result)\n";
-                }
 
                 if ($newResult !== true && isset($report[$result - 1])) {
                     $reportB = $report;
-                    $val = $report[$result - 1];
-                    echo "Trying to remove $val (#" . ($result - 1) . ")\n";
                     unset($reportB[$result - 1]);
                     $newResult = checkReport(array_values($reportB));
-                    if ($newResult === true) {
-                        $val = $report[$result - 1];
-                        echo "Removing $val (#$result)\n";
-                    }
                 }
 
                 if ($newResult !== true) {
-                    echo "No alternative found\n";
                     continue;
                 }
             }
         }
 
-
-        echo "Line is safe\n";
         $saveReports++;
     }
 
-    echo $saveReports;
+    echo $saveReports . "\n";
+
+    if ($saveReports !== 488) {
+        echo "Something went wrong\n";
+    } else {
+        echo "All good\n";
+    }
 });
 
 function checkReport(array $report)
