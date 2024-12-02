a71575e432d0813c490371669cbe55858d264377
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-12-02 23:50:52 +0100

    Refactoring

--- 2024/day2/Red-Nosed ReportsP2.php
+++ 2024/day2/Red-Nosed ReportsP2.php
@@ -10,49 +10,53 @@ runOnInputFile(static function ($file): void
         $report = array_map('intval', explode(' ', trim($line)));
 
         $result = checkReport($report);
-
-        if ($result !== true) {
-            $reportA = $report;
-            unset($reportA[$result]);
-            $newResult = checkReport(array_values($reportA));
-
-            if ($newResult !== true) {
-                $reportB = $report;
-                unset($reportB[$result + 1]);
-                $newResult = checkReport(array_values($reportB));
-
-                if ($newResult !== true && isset($report[$result - 1])) {
-                    $reportB = $report;
-                    unset($reportB[$result - 1]);
-                    $newResult = checkReport(array_values($reportB));
-                }
-
-                if ($newResult !== true) {
-                    continue;
-                }
-            }
+        if (!is_int($result)) {
+            $safeReports++;
+            continue;
         }
 
-        $safeReports++;
+        if (tryToFixReport($result, $report)) {
+            $safeReports++;
+        }
     }
 
-    echo $safeReports . "\n";
-
-    if ($safeReports !== 488) {
-        echo "Something went wrong\n";
-    } else {
-        echo "All good\n";
-    }
+    echo "$safeReports\n";
+    echo $safeReports === 488 ? "All good\n" : "Something went wrong\n";
 });
 
 function checkReport(array $report)
 {
     $orderFactor = $report[0] < $report[1] ? 1 : -1;
 
-    for ($i = 1, $iMax = count($report); $i < $iMax; $i++) {
-        $distance = $orderFactor * ($report[$i] - $report[$i-1]);
+    for ($i = 1, $max = count($report); $i < $max; $i++) {
+        $distance = $orderFactor * ($report[$i] - $report[$i - 1]);
         if ($distance < 1 || $distance > 3) {
             return $i - 1;
+        }
+    }
+
+    return true;
+}
+
+function tryToFixReport(int $result, array $report): bool
+{
+    $reportA = $report;
+    unset($reportA[$result]);
+    $newResult = checkReport(array_values($reportA));
+
+    if ($newResult !== true) {
+        $reportB = $report;
+        unset($reportB[$result + 1]);
+        $newResult = checkReport(array_values($reportB));
+
+        if ($newResult !== true && isset($report[$result - 1])) {
+            $reportB = $report;
+            unset($reportB[$result - 1]);
+            $newResult = checkReport(array_values($reportB));
+        }
+
+        if ($newResult !== true) {
+            return false;
         }
     }
 
