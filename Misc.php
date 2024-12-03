9a3b5a6db10731efae51f4a72731c79f6accc4d6
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-12-03 08:18:05 +0100

    change helper function

--- Misc.php
+++ Misc.php
@@ -4,14 +4,14 @@
  * @param callable $function
  * @return mixed
  */
-function runOnInputFile(callable $function)
+function runOnInputFile(callable $function, $filename = 'input.txt')
 {
-    if (!file_exists('input.txt')) {
+    if (!file_exists($filename)) {
         echo "Input file not found!" . PHP_EOL;
         exit;
     }
 
-    $file = fopen('input.txt', 'r');
+    $file = fopen($filename, 'r');
 
     $return = $function($file);
 
