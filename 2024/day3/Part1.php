12745f6196dfcb9410c3a27ea3b755a292e22504
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-12-03 13:12:01 +0100

    remove comments

--- 2024/day3/Part1.php
+++ 2024/day3/Part1.php
@@ -7,10 +7,7 @@ runOnInputFile(static function ($file): void
 {
     $programString = trim(stream_get_contents($file));
 
-    // find all multiplications
     preg_match_all('/mul\((\d+),(\d+)\)/', $programString, $multiplications, PREG_SET_ORDER);
-
-    // calculate the sum
     $sum = array_sum(array_map(static fn($m) => $m[1] * $m[2], $multiplications));
 
     echo "Solution for Part 1: $sum\n";
