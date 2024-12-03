3a9b6ffb7da4c46c13cbe104d9aa741d6fbbddb5
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-12-03 13:02:44 +0100

    Shorter code

--- 2024/day3/Part1.php
+++ 2024/day3/Part1.php
@@ -5,20 +5,13 @@ require_once './../../Misc.php';
 
 runOnInputFile(static function ($file): void
 {
-    // concatenate all lines of the file
-    $programString = "";
-    while ($line = trim(fgets($file))) {
-        $programString .= $line;
-    }
+    $programString = trim(stream_get_contents($file));
 
     // find all multiplications
     preg_match_all('/mul\((\d+),(\d+)\)/', $programString, $multiplications, PREG_SET_ORDER);
 
     // calculate the sum
-    $sum = 0;
-    foreach ($multiplications as [$_, $a, $b]) {
-        $sum += $a * $b;
-    }
+    $sum = array_sum(array_map(static fn($m) => $m[1] * $m[2], $multiplications));
 
     echo "Solution for Part 1: $sum\n";
 });
