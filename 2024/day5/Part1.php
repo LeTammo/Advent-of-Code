8d77e0a85b79fa8f821d0d354c3ebfdc86124561
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-12-05 21:41:00 +0100

    Solve part 1

--- 2024/day5/Part1.php
+++ 2024/day5/Part1.php
@@ -5,24 +5,32 @@ require_once './../../Misc.php';
 
 runOnInputFile(static function ($file): void {
     $orderRules = [];
-    $notSorted = [];
-    $updates = [];
 
     while (($line = trim(fgets($file))) && $line !== "") {
         [$before, $after] = explode("|", trim($line));
 
-        // TODO: graph erstellen!
+        $orderRules[$before][] = $after;
+        if (!isset($orderRules[$after])) {
+            $orderRules[$after] = [];
+        }
     }
-    var_dump($orderRules, $notSorted);
 
     $correctlyOrderedUpdates = [];
     while (($line = trim(fgets($file)))) {
-        $updates[] = explode(",", trim($line));
+        $update = explode(",", trim($line));
+        for ($i = 0; $i < count($update) - 1; $i++) {
+            if (!in_array($update[$i + 1], $orderRules[$update[$i]])) {
+                continue 2;
+            }
+        }
+        $correctlyOrderedUpdates[] = $update;
     }
 
+    $correctSum = array_sum(array_map(function ($correctlyOrderedUpdate) {
+        return $correctlyOrderedUpdate[(count($correctlyOrderedUpdate) - 1) / 2];
+    }, $correctlyOrderedUpdates));
 
-
-    echo "Solution for Part 1: " . findOrderedMiddlePageSum($orderRules, $updates) . PHP_EOL;
+    echo "Solution for Part 1: " . $correctSum . PHP_EOL;
 }, "example.txt");
 
 function findOrderedMiddlePageSum(array $pageOrderingRules, array $pageUpdateRules): int
