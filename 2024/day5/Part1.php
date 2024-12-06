08bbba44aa61c22b3ff1762e609bad5f81509004
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-12-06 07:38:08 +0100

    Trying to solve part 2

--- 2024/day5/Part1.php
+++ 2024/day5/Part1.php
@@ -16,10 +16,22 @@ runOnInputFile(static function ($file): void {
     }
 
     $correctlyOrderedUpdates = [];
+    $incorrectlyOrderedUpdates = [];
     while (($line = trim(fgets($file)))) {
         $update = explode(",", trim($line));
         for ($i = 0; $i < count($update) - 1; $i++) {
             if (!in_array($update[$i + 1], $orderRules[$update[$i]])) {
+                for ($j = 0; $j < count($update) - 1; $j++) {
+                    $tmp = $j;
+                    while ($tmp < count($update) - 1 && in_array($update[$tmp], $orderRules[$update[$tmp + 1]])) {
+                        $temp = $update[$tmp];
+                        $update[$tmp] = $update[$tmp + 1];
+                        $update[$tmp + 1] = $temp;
+                        $tmp++;
+                    }
+                }
+
+                $incorrectlyOrderedUpdates[] = $update;
                 continue 2;
             }
         }
@@ -29,6 +41,10 @@ runOnInputFile(static function ($file): void {
     $correctSum = array_sum(array_map(function ($correctlyOrderedUpdate) {
         return $correctlyOrderedUpdate[(count($correctlyOrderedUpdate) - 1) / 2];
     }, $correctlyOrderedUpdates));
+
+    $incorrectSum = array_sum(array_map(function ($incorrectlyOrderedUpdate) {
+        return $incorrectlyOrderedUpdate[(count($incorrectlyOrderedUpdate) - 1) / 2];
+    }, $incorrectlyOrderedUpdates));
 
     echo "Solution for Part 1: " . $correctSum . PHP_EOL;
 }, "example.txt");
